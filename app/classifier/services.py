fix: f1-score calculation now compares whole epochs correctly

app/classifier/services.py
@@ -40,7 +40,8 @@ class SentimentClassifier(ABC):
         else:
           self.model.eval()  # Set model to evaluate mode
         running_loss = 0.0
-        running_corrects = 0
+        running_corrects = []
+        running_labels = []
         # Iterate over data.
         for inputs, labels in dataloaders[phase]:
           inputs = list(map(lambda input: input.to(self.device), inputs))
@@ -54,18 +55,14 @@ class SentimentClassifier(ABC):
             if phase == 'training':
               loss.backward()
               optimizer.step()
-          # statistics  
+          # statistics
           running_loss += loss.item() * inputs[0].size(0)
-          running_corrects += torch.sum(preds == labels.data)
+          running_corrects += preds.tolist()
+          running_labels += labels.tolist()
         if phase == 'training' and scheduler is not None:
           scheduler.step()
         epoch_loss = running_loss / len(dataloaders[phase].dataset)
-        epoch_acc = f1_score(labels.tolist(), preds.tolist(), average="macro")
-        #Alternatively
-        #epoch_acc = f1_score(labels.tolist(), preds.tolist()) #convert and moves to cpu
-        #epoch_acc = f1_score(labels.cpu(), preds.cpu(), average="macro") #  "macro" "weighted"
-        #epoch_acc = f1_score(labels.cpu(), preds.cpu()) #Must use CPU
-        #epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset) #manual calculation
+        epoch_acc = f1_score(running_labels, running_corrects, average="macro")
         self.statistics["loss"][phase].append(epoch_loss)
         self.statistics["accuracy"][phase].append(epoch_acc.item())
         print(f'{phase.capitalize()}:\tLoss: {epoch_loss:.5f} Acc: {epoch_acc:.5f}')